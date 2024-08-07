a30398b645ee30e851bda2ce5922e50ae71c1920
javedSwavish <dev2@swavishtek.com>
2024-08-07 11:18:37 +0530

    minor bug fix

--- src/pages/Team/TeamList.jsx
+++ src/pages/Team/TeamList.jsx
@@ -6,6 +6,8 @@ import Filters from "../../components/Filters";
 import TeamCard from "../../components/Team/TeamCard";
 import { useTeamQuery } from "./useQuery/useQuery";
 import InfiniteScrollList from "../../myComponent/InfiniteScrollList";
+import MyContainer from "../../myComponent/MyContainer";
+import { CustomBtn } from "../../myComponent/CustomBtn";
 
 
 const TeamList = () => {
@@ -25,36 +27,61 @@ const TeamList = () => {
   };
   console.log('allTeams', allTeams)
   return (
-    <VStack spacing={4} align="stretch" height="100%" width={"100%"} p={4}>
-      <Box>
-        <Header title="All Team">
-          <Link to={"/teams/add_team"}>
-            <Button colorScheme="brand">Add Team</Button>
-          </Link>
-        </Header>
-        <Filters onSearchChange={setSearch} />
-        <Box maxHeight={"100%"} overflowY="auto" marginEnd={10}>
-          <InfiniteScrollList
-            data={allTeams || []}
-            fetchNextPage={fetchNextPage}
-            hasNextPage={hasNextPage}
-            isFetchingNextPage={isFetchingNextPage}
-            isLoading={isLoading}
-            isFetching={isFetching}
-            renderItem={(item) => (
-              <TeamCard
-                item={item}
-                key={item?._id}
-                onClickUpdate={() => navToUpdateTeam(item)}
-              />
-            )}
-            loadingMessage="Loading teams..."
-            errorMessage="Error fetching teams"
-            noDataMessage="No Teams In The System"
+    <MyContainer
+      header={'All Team'}
+      btnComponent={<>
+        <CustomBtn title={'Add Team'} onClick={() => navigate('/teams/add_team')} />
+      </>}
+    >
+      <InfiniteScrollList
+        data={allTeams || []}
+        fetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage}
+        isFetchingNextPage={isFetchingNextPage}
+        isLoading={isLoading}
+        isFetching={isFetching}
+        renderItem={(item) => (
+          <TeamCard
+            item={item}
+            key={item?._id}
+            onClickUpdate={() => navToUpdateTeam(item)}
           />
-        </Box>
-      </Box>
-    </VStack>
+        )}
+        loadingMessage="Loading teams..."
+        errorMessage="Error fetching teams"
+        noDataMessage="No Teams In The System"
+      />
+    </MyContainer>
+    // <VStack spacing={4} align="stretch" height="100%" width={"100%"} p={4}>
+    //   <Box>
+    //     <Header title="All Team">
+    //       <Link to={"/teams/add_team"}>
+    //         <Button colorScheme="brand">Add Team</Button>
+    //       </Link>
+    //     </Header>
+    //     <Filters onSearchChange={setSearch} />
+    //     <Box maxHeight={"100%"} overflowY="auto" marginEnd={10}>
+    //       <InfiniteScrollList
+    //         data={allTeams || []}
+    //         fetchNextPage={fetchNextPage}
+    //         hasNextPage={hasNextPage}
+    //         isFetchingNextPage={isFetchingNextPage}
+    //         isLoading={isLoading}
+    //         isFetching={isFetching}
+    //         renderItem={(item) => (
+    //           <TeamCard
+    //             item={item}
+    //             key={item?._id}
+    //             onClickUpdate={() => navToUpdateTeam(item)}
+    //           />
+    //         )}
+    //         loadingMessage="Loading teams..."
+    //         errorMessage="Error fetching teams"
+    //         noDataMessage="No Teams In The System"
+    //       />
+    //     </Box>
+    //   </Box>
+    // </VStack>
   );
 };
 
