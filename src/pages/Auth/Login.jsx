238ddad51e110b782a00ba085c9efe35ae846ced
swavishtek-dk <dev11@swavishtek.com>
2024-08-20 12:04:38 +0530

    comment clean

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -28,11 +28,6 @@ const Login = () => {
       console.log('values', values);
       try {
         const { data } = await API_AXIOS.post(`auth/login`, values);
-        // navigate({
-        //   pathname: "/auth/verifyOTP",
-        //   search: `email=${values.email}&pass=${values.password}`,
-
-        // });
         navigate("/auth/verifyOTP", {
           state: {
             email: values.email,
